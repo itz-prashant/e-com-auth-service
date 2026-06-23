2811aca8d9e11f0b798ecf7ab02dced14bb3edc9
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-06-23 18:10:10 +0530

    Refactoring Access and Refresh Token Persistence

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,19 +1,15 @@
-import fs from "node:fs";
-import path from "node:path";
 import { NextFunction, Response } from "express";
 import { RegisterUserRequest } from "../types";
 import { UserService } from "../services/UserService";
 import { Logger } from "winston";
 import { validationResult } from "express-validator";
-import { JwtPayload, sign } from "jsonwebtoken";
-import createHttpError from "http-errors";
-import { CONFIG } from "../config";
-import { AppDataSource } from "../config/data-source";
-import { RefreshToken } from "../entities/RefreshToken";
+import { JwtPayload } from "jsonwebtoken";
+import { TokenService } from "../services/TokenService";
 export class AuthController {
     constructor(
         private userService: UserService,
         private logger: Logger,
+        private tokenService: TokenService,
     ) {}
 
     async register(
@@ -43,50 +39,20 @@ export class AuthController {
             });
             this.logger.info("User has been registered", { id: user.id });
 
-            let privateKey: Buffer;
-
-            try {
-                privateKey = fs.readFileSync(
-                    path.join(__dirname, "../../certs/private.pem"),
-                );
-            } catch {
-                const error = createHttpError(
-                    500,
-                    "Error while read private key",
-                );
-                next(error);
-                return;
-            }
             const payload: JwtPayload = {
                 sub: String(user.id),
                 role: user.role,
             };
 
-            const accessToken = sign(payload, privateKey, {
-                algorithm: "RS256",
-                expiresIn: "1h",
-                issuer: "auth-service",
+            const accessToken = this.tokenService.generateAccessToken(payload);
+
+            const newRefreshToken =
+                await this.tokenService.persistRefreshToken(user);
+
+            const refreshToken = this.tokenService.generateRefreshToken({
+                ...payload,
+                id: String(newRefreshToken.id),
             });
-
-            // persist the refrsh token
-            const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365;
-            const refreshTokenRepo = AppDataSource.getRepository(RefreshToken);
-
-            const newRefreshToken = await refreshTokenRepo.save({
-                user: user,
-                expiresAt: new Date(Date.now() + MS_IN_YEAR),
-            });
-
-            const refreshToken = sign(
-                payload,
-                CONFIG.REFRESH_TOKEN_SECRET as string,
-                {
-                    algorithm: "HS256",
-                    expiresIn: "1yr",
-                    issuer: "auth-service",
-                    jwtid: String(newRefreshToken.id),
-                },
-            );
 
             res.cookie("accessToken", accessToken, {
                 domain: "localhost",
---
+++ src/services/TokenService.ts
@@ -0,0 +1,58 @@
+import createHttpError from "http-errors";
+import { JwtPayload, sign } from "jsonwebtoken";
+import fs from "node:fs";
+import path from "node:path";
+import { CONFIG } from "../config";
+import { User } from "../entities/User";
+import { RefreshToken } from "../entities/RefreshToken";
+import { Repository } from "typeorm";
+
+export class TokenService {
+    constructor(private refreshTokenRepository: Repository<RefreshToken>) {}
+
+    generateAccessToken(payload: JwtPayload) {
+        let privateKey: Buffer;
+
+        try {
+            privateKey = fs.readFileSync(
+                path.join(__dirname, "../../certs/private.pem"),
+            );
+        } catch {
+            const error = createHttpError(500, "Error while read private key");
+            throw error;
+        }
+        const accessToken = sign(payload, privateKey, {
+            algorithm: "RS256",
+            expiresIn: "1h",
+            issuer: "auth-service",
+        });
+
+        return accessToken;
+    }
+
+    generateRefreshToken(payload: JwtPayload) {
+        const refreshToken = sign(
+            payload,
+            CONFIG.REFRESH_TOKEN_SECRET as string,
+            {
+                algorithm: "HS256",
+                expiresIn: "1yr",
+                issuer: "auth-service",
+                jwtid: String(payload.id),
+            },
+        );
+
+        return refreshToken;
+    }
+
+    async persistRefreshToken(user: User) {
+        const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365;
+
+        const newRefreshToken = await this.refreshTokenRepository.save({
+            user: user,
+            expiresAt: new Date(Date.now() + MS_IN_YEAR),
+        });
+
+        return newRefreshToken;
+    }
+}
