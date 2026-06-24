f2c36329466f72a3449d1ec0a787d52cc5ae62da
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-06-24 11:57:54 +0530

    add db connection to server

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -11,7 +11,7 @@ export const AppDataSource = new DataSource({
     username: CONFIG.DB_USERNAME!,
     password: CONFIG.DB_PASSWORD!,
     database: CONFIG.DB_NAME!,
-    synchronize: false, // don't use this in production , Always keep false
+    synchronize: true, // don't use this in production , Always keep false
     logging: false,
     entities: [User, RefreshToken],
     migrations: [],
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import app from "./app";
+import { AppDataSource } from "./config/data-source";
 import { CONFIG } from "./config/index";
 import logger from "./config/logger";
 
-const startServer = () => {
+const startServer = async () => {
     const PORT = CONFIG.PORT;
     try {
+        await AppDataSource.initialize();
+        logger.info("Database connected successfully");
+
         app.listen(PORT, () => {
             logger.info(`Server Listening on PORT ${PORT}`);
         });
@@ -14,4 +18,4 @@ const startServer = () => {
     }
 };
 
-startServer();
+void startServer();
