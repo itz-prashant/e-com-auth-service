4407d90ec0473daf3a6b99c52cb0af2dbd45d4d9
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-07-05 13:56:17 +0530

    add create default admin

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,19 @@ import app from "./app";
 import { AppDataSource } from "./config/data-source";
 import { CONFIG } from "./config/index";
 import logger from "./config/logger";
+import { User } from "./entities/User";
+import { createDefaultAdmin } from "./services/createDefaultAdmin";
+import { UserService } from "./services/UserService";
+
+const userRepository = AppDataSource.getRepository(User);
+const userSerevice = new UserService(userRepository);
 
 const startServer = async () => {
     const PORT = CONFIG.PORT;
     try {
         await AppDataSource.initialize();
         logger.info("Database connected successfully");
-
+        await createDefaultAdmin(userSerevice);
         app.listen(PORT, () => {
             logger.info(`Server Listening on PORT ${PORT}`);
         });
