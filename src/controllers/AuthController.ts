da4d2dfa58f55356b34e0d586d2be59151f1831f
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-07-10 16:24:11 +0530

    improve error handling middleware

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -24,7 +24,7 @@ export class AuthController {
         const result = validationResult(req);
 
         if (!result.isEmpty()) {
-            return res.status(400).json({ errors: result.array() });
+            return next(createHttpError(400, result.array()[0]?.msg as string));
         }
 
         const { firstName, lastName, email, password } = req.body;
@@ -84,7 +84,7 @@ export class AuthController {
         const result = validationResult(req);
 
         if (!result.isEmpty()) {
-            return res.status(400).json({ errors: result.array() });
+            return next(createHttpError(400, result.array()[0]?.msg as string));
         }
 
         const { email, password } = req.body;
